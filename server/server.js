3f70b5b80d5c1fc9c8ac01a506ba146b146cabd8
Nish323 <doragon.nini@icloud.com>
2025-11-18 13:48:40 +0900

    fix archiver

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
+const archiver = require('archiver');
 
 const app = express();
 app.use(cors());
